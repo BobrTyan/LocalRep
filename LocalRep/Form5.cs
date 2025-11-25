3dbf3f58015383461dd1210aae6614956fdc0c75
3vea <vaninakatya78@mail.ru>
2025-11-25 12:50:25 +0300

    форма 5 редактирование

--- LocalRep/Form5.cs
+++ LocalRep/Form5.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,22 @@ namespace LocalRep
         {
             InitializeComponent();
             this.s = student;
-
+            label1.Text = $"{s.name}, набрал(а)";
+            label2.Text = Convert.ToString(s.ball);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            using (StreamWriter writer = new StreamWriter("out.txt", true))
+            {
+                //запись в файл
+                writer.WriteLine(s.name);
+                writer.WriteLine(Convert.ToString(s.ball));
+                writer.WriteLine(Convert.ToString(s.day));
+                writer.WriteLine();
+                writer.Close();
+            }
+            Application.Exit(); // закрыть приложение и все окна выгрузить
         }
     }
 }
