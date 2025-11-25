1670c2e45c09bffa78ef8db971abfa945a0cb0f6
3vea <vaninakatya78@mail.ru>
2025-11-25 12:36:01 +0300

    задание на форме 3

--- LocalRep/Form3.cs
+++ LocalRep/Form3.cs
@@ -13,6 +13,7 @@ namespace LocalRep
     public partial class Form3 : Form
     {
         Student s = new Student();
+        int ball;
         public Form3(Student student)
         {
             InitializeComponent();
@@ -21,6 +22,11 @@ namespace LocalRep
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (checkBox1.Checked == false && checkBox2.Checked == false && checkBox1.Checked == false)
+            { MessageBox.Show("Вы должны выбрать хотя бы 1 ответ!");}
+            if ((checkBox1.Checked == true && checkBox2.Checked == false && checkBox1.Checked == false) || (checkBox1.Checked == false && checkBox2.Checked == true && checkBox1.Checked == false) || (checkBox1.Checked == false && checkBox2.Checked == false && checkBox1.Checked == true)) { ball++; }
+            else if ((checkBox1.Checked == true && checkBox2.Checked == true && checkBox1.Checked == false) || (checkBox1.Checked == true && checkBox2.Checked == false && checkBox1.Checked == true) || (checkBox1.Checked == false && checkBox2.Checked == true && checkBox1.Checked == true)) { ball=ball+2; } 
+            else if (checkBox1.Checked == true && checkBox2.Checked == true && checkBox1.Checked == true) { ball = ball + 3; }
 
         }
     }
