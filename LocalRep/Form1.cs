c17d38065cadc1fcbeef96c0f6c5cffd0a1820a7
3vea <vaninakatya78@mail.ru>
2025-11-25 12:18:09 +0300

    класс студенты

--- LocalRep/Form1.cs
+++ LocalRep/Form1.cs
@@ -16,5 +16,15 @@ namespace LocalRep
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            student.name = textBox1.Text;
+            student.ball = 0;
+            student.day = DateTime.Today;//текущая дата
+            Form2 f = new Form2(student);
+            f.ShowDialog();
+        }
     }
 }
